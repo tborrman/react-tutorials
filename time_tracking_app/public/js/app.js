fc02b88ca2879cd37b527417105ce0e933541b8f
tborrman <tylerborrman@gmail.com>
2018-05-17 19:30:31 -0400

    delete timers

--- time_tracking_app/public/js/app.js
+++ time_tracking_app/public/js/app.js
@@ -47,6 +47,15 @@ class TimersDashboard extends React.Component {
 		});
 	};
 
+	handleTrashClick = (timerId) => {
+		this.deleteTimer(timerId);
+	};
+
+	deleteTimer = (timerId) => {
+		this.setState({
+			timers: this.state.timers.filter(t => t.id !== timerId),
+		});
+	};
 
 	render() {
 		return (
@@ -55,6 +64,7 @@ class TimersDashboard extends React.Component {
 					<EditableTimerList 
 						timers={this.state.timers}
 						onFormSubmit={this.handleEditFormSubmit}
+						onTrashClick={this.handleTrashClick}
 					/>
 					<ToggleableTimerForm
 						onFormSubmit={this.handleCreateFormSubmit}
@@ -76,6 +86,7 @@ class EditableTimerList extends React.Component {
 				elapsed={timer.elapsed}
 				runningSince={timer.runningSince}
 				onFormSubmit={this.props.onFormSubmit}
+				onTrashClick={this.props.onTrashClick}
 			/>
 		));
 		return (
@@ -131,6 +142,7 @@ class EditableTimer extends React.Component {
 					elapsed={this.props.elapsed}
 					runningSince={this.props.runningSince}
 					onEditClick={this.handleEditClick}
+					onTrashClick={this.props.onTrashClick}
 				/>
 			);
 		}
@@ -245,6 +257,10 @@ class ToggleableTimerForm extends React.Component {
 
 class Timer extends React.Component {
 
+	handleTrashClick = () => {
+		this.props.onTrashClick(this.props.id);
+	};
+
 
 	render() {
 	const elapsedString = helpers.renderElapsedString(this.props.elapsed);
@@ -269,7 +285,10 @@ class Timer extends React.Component {
 					>
 						<i className='edit icon' />
 					</span>
-					<span className='right floated trash icon'>
+					<span 
+						className='right floated trash icon'
+						onClick={this.handleTrashClick}
+					>
 						<i className='trash icon' />
 					</span>
 				</div>
